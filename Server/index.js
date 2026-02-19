58552ad1d71a928fc6f69442c08b95e6830f6933
Arpit Jain <aj748694@gmail.com>
2026-02-20 00:54:56 +0530

    Authentication added

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,39 @@
-import express from "express";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+const testRoutes = require("./routes/testRoutes");
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.use(cors());
+app.use(express.json());
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+
+mongoose.connect("mongodb://localhost:27017")
+.then(() => {
+    console.log("MongoDB Connected Successfully");
+})
+.catch((err) => {
+    console.log("MongoDB Connection Error:");
+    console.log(err.message);
+});
+
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/test", testRoutes);
+
+app.get("/", (req, res) => {
+    res.send("SDAS Backend Running");
+});
+
+const PORT = process.env.PORT || 3000;
+ 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
